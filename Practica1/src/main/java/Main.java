9483d4230fa71c6bcfdb252324392b4d21662ba7
Pablo <pablo.pique01@estudiant.upf.edu>
2019-01-26 15:57:40 +0100

    project creation

--- Practica1/src/main/java/Main.java
+++ Practica1/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hola");
+
+    public static void main(String args[]){
+        Splitter s = new Splitter() {
+            @Override
+            public List<String> split(long sizeBytes, List<Path> files) throws IOException {
+
+                return null;
+            }
+        };
+        
+
+
     }
 }
--- Practica1/src/main/java/Splitter.java
+++ Practica1/src/main/java/Splitter.java
@@ -1,3 +1,8 @@
-public class Splitter {
-}
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
 
+public interface Splitter {
+
+    List<String> split(long sizeBytes, List<Path> files) throws IOException;
+}
